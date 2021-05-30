2593dfb043eb238267bd52f47f0346fd77cc70a6
ashwini2007 <74718589+ashwini2007@users.noreply.github.com>
2021-05-30 11:12:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var score = 0;
+
+function preload(){
+  
+  bgImg = loadImage("track.png")
+}
+
+function setup() {
+  createCanvas(1300, 650);
+  
+ 
+  
+  
+  
+}
+
+function draw() {
+ background(bgImg);
+ drawSprites();
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
